ac6183f516ec72c5855a690447adc5c35b5a4af1
Abhinav7997 <abhinavreddymalkannagari@example.com>
2024-01-24 10:40:22 -0600

    Constructor

---
+++ src/main/java/staticFinal/Chef.java
@@ -0,0 +1,16 @@
+package Keyword;
+
+
+public class Chef {
+    static int chefnumber = 56786;
+
+    public static int getChefnumber() {
+        return chefnumber;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getChefnumber());
+    }
+
+}
